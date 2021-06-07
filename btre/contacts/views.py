0d7d650744f1a2015d4ec33ebf3d251d33a57e34
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-06-08 02:51:36 +0300

    project upload

---
+++ btre/contacts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def contact(request):
+    return
+
+    # contact.save()
+
+    # send_mail(
+    #     'Property Listing Inquiry',
+    #     'There has been Inquiry for ' + listing + ' Sign into the admin panel for more info',
+    #     'travesy.brad@gmail.com',
+    #     [realtor_email, 'techguyinfo@gmail.com'],
+    #     fail_silently=False
+    #     )
+
+    #     messages.success(request, 'Your request has been submitted, a realtor, a realtor will get back to you soon')
+    #     return redirect('/listings/'+listing_id)
---
+++ btre/pages/views.py
@@ -0,0 +1,31 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from listings.choices import bedroom_choices, state_choices, price_choices
+
+from listings.models import Listing
+from django.shortcuts import render
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors,
+    }
+
+    return render(request, 'about.html', context)
+
