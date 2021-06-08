4f0d7ecc247cec2a53542291d0f4d1721bde4862
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-06-08 18:20:19 +0300

    main_photo + listings tables

--- btre/listings/views.py
+++ btre/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
 from listings.choices import bedroom_choices, state_choices, price_choices
